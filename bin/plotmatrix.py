212c1cc905ff7558f0cf3c476d3e0cf59bd64b25
snolfi <36332910+snolfi@users.noreply.github.com>
2022-05-17 14:27:47 +0200

    Update plotmatrix.py
    Added the possibility to plot the average of multiple matrix files

--- bin/plotmatrix.py
+++ bin/plotmatrix.py
@@ -17,15 +17,26 @@ import os
 
 print("plotmatrix.py")
 print("plot the data contained in a *.npy matrix file as a colormap")
-print("requires the name of the file as a parameter")
+print("requires the name of the file or of the files as a parameter")
+print("in case of multiple files, it plots the average data")
 print("")
 
 
 if len(sys.argv) == 2:
     matrix = np.load(sys.argv[1])
-    plt.rcParams["figure.figsize"] = [7.00, 3.50]
-    plt.rcParams["figure.autolayout"] = True
-    #im = plt.imshow(matrix, cmap="copper_r")
-    im = plt.imshow(matrix, cmap="bwr")
-    plt.colorbar(im)
-    plt.show()
+else:
+    if len(sys.argv) > 2:
+        cmatrix = np.load(sys.argv[1])
+        matrix = np.zeros(cmatrix.shape)
+        nmatrix = 0
+        for f in sys.argv[1:]:
+            cmatrix = np.load(f)
+            matrix = matrix + cmatrix
+            nmatrix = nmatrix + 1
+        matrix = matrix / float(nmatrix)
+plt.rcParams["figure.figsize"] = [7.00, 3.50]
+plt.rcParams["figure.autolayout"] = True
+#im = plt.imshow(matrix, cmap="copper_r")
+im = plt.imshow(matrix, cmap="bwr")
+plt.colorbar(im)
+plt.show()
